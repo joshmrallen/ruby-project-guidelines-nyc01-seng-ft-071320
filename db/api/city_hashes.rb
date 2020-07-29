4d2fa96ec65c1660a8ac84a7c961a82a54c7dcec
stackguardian <mferdous98@aol.com>
2020-07-29 18:27:05 -0400

    added run methods to seeds file to test and NEED TO MAKE SEED METHOD FOR CITYHASHES TO LOCATIONS TABLE

--- db/api/city_hashes.rb
+++ db/api/city_hashes.rb
@@ -1,67 +1,71 @@
 #hashes of latitude and longitude box for each city accepted by the api
 #TODO: add country attr, and city attr
 
-BANGALORE = {
-    :city => "Bangalore",
-    :country => "India",
-    :north => 13.023577,
-    :west => 77.536856,
-    :south => 12.923210,
-    :east => 77.642256
-}
-
-BARCELONA = {
-    :name => "Barcelona, Spain",
-    :north => 41.42,
-    :west => 2.11,
-    :south => 41.347463,
-    :east => 2.228208
-}
-
-BERLIN = {
-    :name => "Berlin, Germany",
-    :north => 52.541755,
-    :west => 13.354201,
-    :south => 52.490569,
-    :east => 13.457198
-}
-
-DALLAS = {
-    :name => "Dallas, TX, USA",
-    :north => 32.806993,
-    :west => -96.836857,
-    :south => 32.740310,
-    :east => -96.737293
-}
-
-LONDON = {
-    :name => "London, UK",
-    :north => 51.520180,
-    :west => -0.169882,
-    :south => 51.484703,
-    :east => -0.061048
-}
-
-NEW_YORK = {
-    :name => "New York, NY, USA",
-    :north => 40.792027,
-    :west => -74.058204,
-    :south => 40.697607,
-    :east => -73.942847
-}
-
-PARIS = {
-    :name => "Paris, France",
-    :north => 48.91,
-    :west => 2.25,
-    :south => 48.80,
-    :east => 2.46
-}
-
-SAN_FRANCISCO = {
-    :name => "San Francisco, CA, USA",
-    :north => 37.810980,
-    :west => -122.483716,
-    :south => 37.732007,
-    :east => -122.370076
-}
+def city_hash
+    {
+        "Bangalore" => {
+            :code => "BLR",
+            :country => "India",
+            :north => 13.023577,
+            :west => 77.536856,
+            :south => 12.923210,
+            :east => 77.642256
+        },
+        "Barcelona" => {
+            :code => "BCN",
+            :county => "Spain",
+            :north => 41.42,
+            :west => 2.11,
+            :south => 41.347463,
+            :east => 2.228208
+        },
+        "Berlin" => {
+            :code => "TXL",
+            :country => "Germany",
+            :north => 52.541755,
+            :west => 13.354201,
+            :south => 52.490569,
+            :east => 13.457198
+        },
+        "Dallas" => {
+            :code => "DFW",
+            :country => "USA",
+            :north => 32.806993,
+            :west => -96.836857,
+            :south => 32.740310,
+            :east => -96.737293
+        },
+        "London" => { 
+            :code => "LHR",
+            :country => "UK",
+            :north => 51.520180,
+            :west => -0.169882,
+            :south => 51.484703,
+            :east => -0.061048
+        },
+        "New York City" => {
+            :code => "JFK",
+            :country => "USA",
+            :north => 40.792027,
+            :west => -74.058204,
+            :south => 40.697607,
+            :east => -73.942847
+        },
+        "Paris" => {
+            :code => "CDG",
+            :country => "France",
+            :north => 48.91,
+            :west => 2.25,
+            :south => 48.80,
+            :east => 2.46
+        },
+        "San Francisco" => { 
+            :code => "SFO",
+            :country => "USA",
+            :north => 37.810980,
+            :west => -122.483716,
+            :south => 37.732007,
+            :east => -122.370076
+        }
+    }
+end
