5f06c2e9a3695177a8d49443a6c7533a4357772b
github username <joshmrallen@gmail.com>
2020-07-31 08:12:19 -0400

    fixed some loop errors; added string parsing to slow months; added slow_months method to location class

--- app/models/location.rb
+++ app/models/location.rb
@@ -14,4 +14,44 @@ class Location < ActiveRecord::Base
         sights.each {|sight| self.sights << Sight.create(name: sight)}
     end
 
+    def get_slow_months
+        months = AmadeusAPI.slow_season(city_hash[self.city][:code])
+        if !months.empty?
+            numbers = months.map do |month|
+                month.delete_prefix("2017-")
+            end
+            long_form = numbers.map do |month|
+                case month
+                when "01"
+                    month = "January"
+                when "02"
+                    month = "February"
+                when "03"
+                    month = "March"
+                when "04"
+                    month = "April"
+                when "05"
+                    month = "May"
+                when "06"
+                    month = "June"
+                when "07"
+                    month = "July"
+                when "08"
+                    month = "August"
+                when "09"
+                    month = "September"
+                when "10"
+                    month = "October"
+                when "11"
+                    month = "November"
+                when "12"
+                    month = "December"
+                end
+            end
+            return long_form
+        else
+            return months
+        end
+    end #slow_months instance method
+
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,33 @@
 require_relative '../config/environment'
 
+def banner
+    system("clear")
+    puts "                                                                                                                         
+                                                                                                                         
+    VVVVVVVV           VVVVVVVV                                                            tttt                              
+    V::::::V           V::::::V                                                         ttt:::t                              
+    V::::::V           V::::::V                                                         t:::::t                              
+    V::::::V           V::::::V                                                         t:::::t                              
+     V:::::V           V:::::Vaaaaaaaaaaaaa      cccccccccccccccc  aaaaaaaaaaaaa  ttttttt:::::ttttttt        eeeeeeeeeeee    
+      V:::::V         V:::::V a::::::::::::a   cc:::::::::::::::c  a::::::::::::a t:::::::::::::::::t      ee::::::::::::ee  
+       V:::::V       V:::::V  aaaaaaaaa:::::a c:::::::::::::::::c  aaaaaaaaa:::::at:::::::::::::::::t     e::::::eeeee:::::ee
+        V:::::V     V:::::V            a::::ac:::::::cccccc:::::c           a::::atttttt:::::::tttttt    e::::::e     e:::::e
+         V:::::V   V:::::V      aaaaaaa:::::ac::::::c     ccccccc    aaaaaaa:::::a      t:::::t          e:::::::eeeee::::::e
+          V:::::V V:::::V     aa::::::::::::ac:::::c               aa::::::::::::a      t:::::t          e:::::::::::::::::e 
+           V:::::V:::::V     a::::aaaa::::::ac:::::c              a::::aaaa::::::a      t:::::t          e::::::eeeeeeeeeee  
+            V:::::::::V     a::::a    a:::::ac::::::c     ccccccca::::a    a:::::a      t:::::t    tttttte:::::::e           
+             V:::::::V      a::::a    a:::::ac:::::::cccccc:::::ca::::a    a:::::a      t::::::tttt:::::te::::::::e          
+              V:::::V       a:::::aaaa::::::a c:::::::::::::::::ca:::::aaaa::::::a      tt::::::::::::::t e::::::::eeeeeeee  
+               V:::V         a::::::::::aa:::a cc:::::::::::::::c a::::::::::aa:::a       tt:::::::::::tt  ee:::::::::::::e  
+                VVV           aaaaaaaaaa  aaaa   cccccccccccccccc  aaaaaaaaaa  aaaa         ttttttttttt      eeeeeeeeeeeeee  
+                                                                                                                             
+                                                                                                                             
+                                                                                                                             
+                                                                                                                             
+                                                                                                                             
+                                                                                                                             
+                                                                                                                             "
+end
 
 def cities
     [
@@ -11,6 +39,7 @@ def cities
         "New York City", 
         "Paris", 
         "San Francisco",
+        # "Sign Out",
         "Exit"
     ]
 end
@@ -27,7 +56,6 @@ def favorite(user, location, prompt)
 end
 
 def welcome_message
-    # add banner here
     get_name
 end
 
@@ -37,37 +65,45 @@ def features
         "Sights to See",
         "Hotels to Stay At",
         "My Favorite Destinations",
-        "Other Destinations",
         "Exit"
     ]
 end
 
 
-def run(features)
-    system("clear")
+def run
     prompt = TTY::Prompt.new
+    banner
     current_user = welcome_message
 
     choice = 0
     city_choice = 0
     while city_choice != cities[8]
-        city_choice = prompt.select("\nPlease choose your destination.\n", cities, per_page: 9)
+        choice = 0
+        city_choice = prompt.select("\nPlease choose your destination.\n", cities, per_page: 10)
+        # hash_key = city_choice.to_s
+        # while city_choice != cities[8] #infinite loop after features while loop exits
+            #this if statement seems to only run once, only allowing the inner while loop to run for just one city_choice value
 
-        if city_choice == "Exit"
-            puts "Have a nice day!"
-        else
-            city = city_hash[city_choice]
-            city_code = city_hash[city_choice][:code] # upon Exit, throws NoMethodError '[]' for nil:NilClass because city_hash[city_choice] returns 'nil'
-            location = Location.find_by(city: city_choice)
-            #location not updating with city_choice -- why?
+            # if city_choice == "Exit"
+            #     puts "Have a nice day!"
+            # else
+                # city = city_hash[city_choice]
+                # city_code = city_hash[city_choice][:code] # upon Exit, throws NoMethodError '[]' for nil:NilClass because city_hash[city_choice] returns 'nil'
+                # location = Location.find_by(city: city_choice)
+                #location not updating with city_choice -- why?
+            # end
 
-            while choice != features[5]
+            while choice != features[4]
+                city = city_hash[city_choice]
+                city_code = city_hash[city_choice][:code]
+                location = Location.find_by(city: city_choice)
+
                 choice = prompt.select("\nWhat would you like to see?\n", features)
-    
+
                 case choice
                 when features[0] #best times for travel
-                    months = AmadeusAPI.slow_season(city_code)
-                    if months == []
+                    months = location.get_slow_months
+                    if months.empty?
                         puts "\nYou're in luck! That location is slow all year-round.\n"
                     else
                         puts "\nHere are the best months to travel to #{city_choice}:\n"
@@ -75,7 +111,7 @@ def run(features)
                     end
                     favorite(current_user, location, prompt)
                 when features[1] #sights to see
-                    puts "\nHere are a few place to visit in #{city_choice}:\n\n"
+                    puts "\nHere are a few places to visit in #{city_choice}:\n\n"
                     sights = location.get_sights
                     sights.each {|sight| puts sight}
                     favorite(current_user, location, prompt)
@@ -88,27 +124,25 @@ def run(features)
                         hotels.each {|hotel| puts hotel} # Bangalore currently returning empty array
                         favorite(current_user, location, prompt)
                     end
-                when features [3] #my favorite destinations -- ERROR - favorites only saves the first destination choice -- location probably isn't updating when city_choice is called
+                when features[3] #my favorite destinations -- ERROR - favorites only saves the first destination choice -- location probably isn't updating when city_choice is called
                     puts "\n Here's a list of your favorite destinations:\n"
                     if current_user.locations.empty?
                         puts "\nYou don't have any favorites yet!\n"
-                        city_choice = prompt.select("\nPlease choose your destination.\n", cities, per_page: 9)
+                        # city_choice = prompt.select("\nPlease choose your destination.\n", cities, per_page: 9)
                     else
-                        current_user.locations.each {|location| puts "\n#{location.city}\n"}
+                        current_user.locations.uniq.each {|location| puts "\n#{location.city}\n"}
                     end
-                when features [4] #other destinations
-                    city_choice = prompt.select("\nPlease choose your destination.\n", cities, per_page: 9)
-                    # location = Location.find_by(city: city_choice)
                 else
-                    # choice = prompt.select("\nWhat would you like to see?\n", features)
+                    choice = features[4]
                 end
+
+            end #inner while
             
-            end
-             # throws undefined NoMethod error when Exit is chosen in outer while loop
-        end
+        # end #while not sign out
+        
         # binding.pry
-    end     
+    end #outer while - while not exit
+binding.pry
+end #run method definition
 
-end
-
-run(features)
+run
